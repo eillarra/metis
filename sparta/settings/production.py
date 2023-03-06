a4dc26d22ccbd61a5b6ed40598a4aa131ea3ccc0
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2023-03-06 14:30:27 +0100

    chore: update sentry config

--- sparta/settings/production.py
+++ sparta/settings/production.py
@@ -1,4 +1,9 @@
 import os
+import sentry_sdk
+
+from django.core.exceptions import DisallowedHost
+from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
 
 from .base import *  # noqa
 
@@ -19,6 +24,22 @@ CSRF_USE_SESSIONS = True
 
 SENDFILE_ROOT = f"{MEDIA_ROOT}private"
 SENDFILE_URL = "/-internal"
+
+
+# https://docs.sentry.io/platforms/python/guides/django/
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN", None),
+    release=os.environ.get("GIT_COMMIT_HASH", None),
+    environment="production",
+    integrations=[DjangoIntegration(), RedisIntegration()],
+    ignore_errors=[DisallowedHost],
+    send_default_pii=True,
+    traces_sample_rate=0.1,
+    _experiments={
+        "profiles_sample_rate": 0.1,
+    },
+)
 
 
 # https://docs.djangoproject.com/en/dev/topics/cache/
--- sparta/wsgi.py
+++ sparta/wsgi.py
@@ -1,26 +1,13 @@
 # https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 
 import os
-import sentry_sdk
 
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
-from sentry_sdk.integrations.django import DjangoIntegration
-from sentry_sdk.integrations.logging import ignore_logger
 from whitenoise import WhiteNoise
 
 
 env = os.environ.get("DJANGO_ENV", None)
-
-if env in ["production"]:
-    sentry_sdk.init(
-        dsn=os.environ.get("SENTRY_DSN", "SENTRY_DSN"),
-        release=os.environ.get("GIT_REV", None),
-        environment=env,
-        integrations=[DjangoIntegration()],
-        send_default_pii=True,
-    )
-    ignore_logger("django.security.DisallowedHost")
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sparta.settings")
 os.environ["HTTPS"] = "on"
