c498b0aa3dc184b8a64219c9389316cd9d360a35
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2023-05-08 13:59:26 +0200

    fix: update domain for sentry/frontend

--- vue/src/boot.ts
+++ vue/src/boot.ts
@@ -66,7 +66,7 @@ const bootApp = (routes: RouteRecordRaw[]) => {
           integrations: [
             new Sentry.BrowserTracing({
               routingInstrumentation: Sentry.vueRouterInstrumentation(Router),
-              tracePropagationTargets: ['localhost', 'tw06v070.ugent.be', /^\//],
+              tracePropagationTargets: ['localhost', 'metis.ugent.be', /^\//],
             }),
           ],
           tracesSampleRate: 0.1,
