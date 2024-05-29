84f5f5ebb24c249436db82d218cc4c23775f100d
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2024-05-29 14:15:22 +0200

    fix: ignore 'nvt' values in gezbev calculator

--- metis/services/evaluator/gezbev.py
+++ metis/services/evaluator/gezbev.py
@@ -10,7 +10,6 @@ class GezbevEvaluator(Evaluator):
         """Evaluate the model on the given data.
 
         We calculate how many items we have to evaluate.
-        TODO: do we ignore the nvt answers?
         We assign the maximun score to each item, so we know the maximum score possible.
         We calculate the final score by dividing the sum of the scores by the maximum score,
         and normalizing the result to a 45 point scale.
@@ -25,10 +24,21 @@ class GezbevEvaluator(Evaluator):
         max_points = 0
         points = 0
 
-        for _, section_data in self.evaluation.data["sections"].items():
-            for _, item_data in section_data["scores"].items():
-                max_points += max_score
-                p = scores[item_data[0]]["points"]
-                points += p if p else 0
+        # make a dict of the scores for easier access
+        evaluation_scores = {}
+
+        for section_code, section_data in self.evaluation.data["sections"].items():
+            for item_code, item_data in section_data["scores"].items():
+                evaluation_scores[(section_code, item_code)] = scores[item_data[0]]["points"]
+
+        # go through the evaluation form and calculate the score
+
+        for section in self.evaluation_form.definition["sections"]:
+            for item in section["items"]:
+                p = evaluation_scores.get((section["code"], item["value"]), None)
+
+                if p is not None:  # ignore 'nvt' scores
+                    max_points += max_score
+                    points += p
 
         return round((points / max_points) * 45, 2)
