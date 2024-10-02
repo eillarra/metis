366ee18a89a92f32eb2dbddb4104a141f8f36ced
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2024-10-02 16:14:26 +0200

    chore: pass country_code to geocoder

--- metis/services/mapbox.py
+++ metis/services/mapbox.py
@@ -4,6 +4,8 @@ import requests
 
 
 class MapboxFeature:
+    """A class to represent a Mapbox feature."""
+
     def __init__(self, feature: dict):
         self._raw = feature
 
@@ -18,45 +20,57 @@ class MapboxFeature:
 
     @property
     def address(self) -> str:
+        """The street address."""
         return self._raw["place_name"].split(",")[0]
 
     @property
     def full_address(self) -> str:
+        """The full address."""
         return self._raw["place_name"]
 
     @property
     def city(self) -> str | None:
+        """The city name."""
         return self.__get_context("place", field="text")
 
     @property
     def postcode(self) -> str | None:
+        """The postal code."""
         return self.__get_context("postcode", field="text")
 
     @property
     def region(self) -> dict | None:
+        """The region."""
         return self.__get_context("region")
 
     @property
     def country(self) -> dict | None:
+        """The country."""
         return self.__get_context("country")
 
     @property
     def coordinates(self) -> list:
+        """The coordinates."""
         return self._raw["geometry"]["coordinates"]
 
     @property
     def latitude(self) -> float:
+        """The latitude."""
         return self.coordinates[1]
 
     @property
     def longitude(self) -> float:
+        """The longitude."""
         return self.coordinates[0]
 
     def to_dict(self) -> dict:
+        """Return the raw feature as a dictionary."""
         return self._raw
 
 
 class Mapbox:
+    """A class to interact with the Mapbox API."""
+
     access_token = os.environ.get("MAPBOX_TOKEN")
     api_endpoint = "https://api.mapbox.com"
     api_version = "v5"
@@ -74,12 +88,19 @@ class Mapbox:
     def __exit__(self, exc_type, exc_value, traceback):
         self.session.close()
 
-    def geocode(self, address: str) -> MapboxFeature | None:
+    def geocode(self, address: str, country_code: str = "BE") -> MapboxFeature | None:
+        """Geocode an address using the Mapbox API.
+
+        :param address: The address to geocode.
+        :param country_code: The country code.
+        :return: The geocoded feature or None.
+        :raises Exception: If the API request fails.
+        """
         try:
             url = f"{self.api_endpoint}/geocoding/{self.api_version}/mapbox.places/{address}.json"
             params = {
                 "access_token": self.access_token,
-                "country": "be",
+                "country": country_code.lower(),
                 "types": "address",
                 "limit": 1,
                 "language": "nl,en",
